2ea518cfc1c46e4559736e9dc8e2bddb6527ab39
Santiago Puerta <puertasantiago2002@gmail.com>
2023-05-05 00:32:56 -0500

    implementacion de notificador de cambios

--- src/comparator.py
+++ src/comparator.py
@@ -1,10 +1,22 @@
+from trello.singles.card import Card
 
-@classmethod
-def compareCardsLists(cls, dbCardsList:list, trelloCardsList:list) -> list:
+
+def compareCardsLists(dbCardsList:list, trelloCardsList:list) -> list:
+    assert isinstance(dbCardsList, list) and isinstance(trelloCardsList, list), "Both atributes must be lists"
     idsDbCardsList = [card1.getCardId() for card1 in dbCardsList]
     idsTrelloCardsList = [card2.getCardId() for card2 in trelloCardsList]
-    missingIdsInDb:list = list(set(idsTrelloCardsList) - set(idsDbCardsList)) #lista de Ids de cards por añadir a la base de datos 
-    return missingIdsInDb
+    missingCardsIdsInDb:list = list(set(idsTrelloCardsList) - set(idsDbCardsList)) #lista de Ids de cards por añadir a la base de datos 
+    return missingCardsIdsInDb
+
+
+def notifyChanges(missingCardsIdsInDb:list) -> str:
+    if len(missingCardsIdsInDb) == 0:
+        return "No changes to resolve\n"
+    message = "Here is the list of changes to resolve.\nAdd(+)\n"
+    card:Card
+    for i,card in enumerate(missingCardsIdsInDb):
+        message += f"{i}. {card.getCardName()}\n"
+    return message
     
         
         
